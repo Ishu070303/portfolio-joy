7daef90bc90aeb5d82bf406d6dfa8e78cc81e568
Eshika rawat <eshikarawat02@gmail.com>
2024-03-22 13:09:08 +0530

    added usepathname

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
+const NavLink = ({ link }) => {
+  const pathName = usePathname();
+
+  return (
+    <Link className={`rounded p-1 ${pathName === link.link && "bg-black text-white"}`} href={link.link}>{link.title}</Link>
+  )
+}
+
+export default NavLink
