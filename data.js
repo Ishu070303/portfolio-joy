723ea657a3245a9b3a7b7524f589af6f3cc2a676
Eshika rawat <eshikarawat02@gmail.com>
2024-03-30 16:42:39 +0530

    added skills

---
+++ data.js
@@ -0,0 +1,60 @@
+export const skills = [
+    {
+        id: 1,
+        skill: "Javascript",
+    },
+
+    {
+        id: 2,
+        skill: "HTML & CSS",
+    },
+
+    {
+        id: 3,
+        skill: "React",
+    },
+
+    {
+        id: 4,
+        skill: "NextJs",
+    },
+
+    {
+        id: 5,
+        skill: "Material UI",
+    },
+
+    {
+        id: 6,
+        skill: "Tailwind CSS",
+    },
+
+    {
+        id: 7,
+        skill: "NodeJs",
+    },
+    {
+        id: 8,
+        skill: "ExpressJs",
+    },
+    {
+        id: 9,
+        skill: "MongoDB",
+    },
+    {
+        id: 10,
+        skill: "Git",
+    },
+    {
+        id: 11,
+        skill: "Github",
+    },
+    {
+        id: 12,
+        skill: "NPM",
+    },
+    {
+        id: 13,
+        skill: "Appwrite",
+    }
+]
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { skills } from "../../../data";
 
 const Aboutpage = () => {
   return (
@@ -16,8 +17,7 @@ const Aboutpage = () => {
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
           {/* Biograpgy Container */}
           <div className="flex flex-col gap-12 justify-center">
-<<<<<<< HEAD
-            <h1 className="font-bold text-2xl">BIOGRAPGY</h1>
+            <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
             <p className="text-lg">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod
               esse impedit, eum aliquid, itaque doloribus, animi dignissimos
@@ -40,21 +40,40 @@ const Aboutpage = () => {
               height={50}
             >
               <path
-                d="M12 2C17.5229 2 22 9.37156 22 12C22 14.6284 17.5229 22 12 22C6.47715 22 2 14.6284 2 12C2 9.37156 6.47715 2 12 2Z"
+                d="M12 2C16.9639 2 21.112 6.18483 21.112 11.0407C21.112 15.8965 16.9639 22 12 22C7.03607 22 2.888 15.8965 2.888 11.0407C2.888 6.18483 7.03607 2 12 2Z"
                 stroke="#000000"
                 strokeWidth="1"
               ></path>
               <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
               <path d="M15 11L12 14L9 11" stroke="#000000" strokeWidth="1"></path>
             </svg>
-=======
-            <h1>BIOGRAPGY</h1>
-            <p></p>
->>>>>>> c937e28a0609c17daa0aa4136722ec559aa86e8c
           </div>
 
           {/* Skills */}
-          <div className="">Skills</div>
+          <div className="flex flex-col gap-12 justify-center">
+            <h1 className="font-bold text-2xl">SKILLS</h1>
+
+            <div className="flex gap-4 flex-wrap">
+              { skills.map((skill) => (
+                <div key={skill.id} className="rounded p-2 bg-black cursor-pointer text-white hover:bg-white hover:text-black">{skill.skill}</div>
+              ))}
+            </div>
+
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M12 2C16.9639 2 21.112 6.18483 21.112 11.0407C21.112 15.8965 16.9639 22 12 22C7.03607 22 2.888 15.8965 2.888 11.0407C2.888 6.18483 7.03607 2 12 2Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path d="M15 11L12 14L9 11" stroke="#000000" strokeWidth="1"></path>
+            </svg>
 
           {/* Experience */}
           <div className="">Experience</div>
@@ -62,6 +81,7 @@ const Aboutpage = () => {
 
         {/* Svg Container */}
         <div className="hidden"></div>
+      </div>
       </div>
     </motion.div>
   );
